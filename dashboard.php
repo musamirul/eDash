acd9cfc42177b2595db3d77434f2546a359fa83f
ameirul mustaqim <46254149+musamirul@users.noreply.github.com>
2023-09-08 17:31:08 +0800

    update dash

---
+++ dashboard.php
@@ -0,0 +1,90 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="static/bootstrap/css/bootstrap.min.css" media="screen">
+  </head>
+<body>
+<div class="container">
+    <!--date and day-->
+    <div class="row bg-dark text-white">
+        <div class="d-flex justify-content-end">
+            <div class="p-1"><b>DATE</b> : 8/9/2023</div>
+            <div class="p-1"><b>Day</b> : Friday</div>
+        </div>
+    </div>
+
+    <!--content-->
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col"><b>CONSULTANT ON CALL</b></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">ANAESTHETIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex flex-column mb-3">
+                        <div class="p-1"><b>CONSULTANT ON CALL</b></div>
+                        <div class="p-1">ANAESTHETIST</div>
+                        <div class="P-1">RADIOLOGISTGEON</div>
+                        <div class="p-1">UROLOGISTGEON</div>
+                        <div class="p-1">PSYCHIATRISTGEON</div>
+                        <div class="p-1">CARDIOLOGIST</div>
+                        <div class="p-1">EAR, NOSE & THROAT SURGEON</div>
+                        <div class="p-1">GENERAL PHYSICIAN</div>
+                        <div class="p-1">GENERAL SURGEON</div>
+                        <div class="p-1">NEPHROLOGIST</div>
+                        <div class="p-1">GASTROENTEROLOGIST</div>
+                        <div class="p-1">NEUROSURGEON</div>
+                        <div class="p-1">OBSTETRICIAN & GYNAECOLOGIST</div>
+                        <div class="p-1">OPHTHALMOLOGIST</div>
+                        <div class="p-1">ORTHOPAEDIC SURGEON</div>
+                        <div class="p-1">PAEDIATRICIAN</div>
+                        <div class="p-1">PLASTIC SURGEON</div>
+                        <div class="p-1">RADIOLOGIST</div>
+                        <div class="p-1">UROLOGIST</div>
+                        <div class="p-1">PSYCHIATRIST</div>
+                        <div class="row mb-3">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">EmailTETE</label>
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+                            </div>
+                          </div>
+                          <div class="row mb-3">
+                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">EmailTASDFASDFETE</label>
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
+                            </div>
+                          </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="d-flex flex-column mb-3">
+                        <div class="p-1">.</div>
+                        <div class="p-1">Flex item 2</div>
+                        <div class="p-1">Flex item 3</div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="col">
+            
+        </div>
+    </div>
+
+
+</div>
+
+
+
+<footer class="text-center text-white fixed-bottom bg-dark mt-5">
+    <div class="text-center p-2" style="background-color:rgba(0,0,0,0.2);">IT Services @ 2023 Copyright :<a class="text-white text-decoration-none" href="#">A&E Dashboard</a></div>
+</footer>
+    <script src="static/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="Interface/style/jQuery/jquery-3.6.0.min.js"></script>
+  </body>
+</html>
