645be12294504146a0d0d3f7f9958287c25d0c64
ameirul mustaqim <46254149+musamirul@users.noreply.github.com>
2023-09-10 19:17:04 +0800

    update dashboard

--- dashboard.php
+++ dashboard.php
@@ -10,69 +10,188 @@
     <div class="row bg-dark text-white">
         <div class="d-flex justify-content-end">
             <div class="p-1"><b>DATE</b> : 8/9/2023</div>
-            <div class="p-1"><b>Day</b> : Friday</div>
+            <div class="p-1"><b>DAY</b> : Friday</div>
         </div>
     </div>
-
     <!--content-->
-    <div class="row">
-        <div class="col">
+    <div class="row pt-2">
+        <div class="col border shadow-sm rounded me-3">
             <div class="row">
-                <div class="col"><b>CONSULTANT ON CALL</b></div>
+                <div class="col fs-4"><b>CONSULTANT ON CALL</b></div>
             </div>
-            <div class="row">
-                <div class="col-sm-6">ANAESTHETIST</div>
+            <div class="row pt-2">
+                <div class="col-sm-6 fw-semibold">ANAESTHETIST</div>
                 <div class="col-sm-6">Dr Anand</div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <div class="d-flex flex-column mb-3">
-                        <div class="p-1"><b>CONSULTANT ON CALL</b></div>
-                        <div class="p-1">ANAESTHETIST</div>
-                        <div class="P-1">RADIOLOGISTGEON</div>
-                        <div class="p-1">UROLOGISTGEON</div>
-                        <div class="p-1">PSYCHIATRISTGEON</div>
-                        <div class="p-1">CARDIOLOGIST</div>
-                        <div class="p-1">EAR, NOSE & THROAT SURGEON</div>
-                        <div class="p-1">GENERAL PHYSICIAN</div>
-                        <div class="p-1">GENERAL SURGEON</div>
-                        <div class="p-1">NEPHROLOGIST</div>
-                        <div class="p-1">GASTROENTEROLOGIST</div>
-                        <div class="p-1">NEUROSURGEON</div>
-                        <div class="p-1">OBSTETRICIAN & GYNAECOLOGIST</div>
-                        <div class="p-1">OPHTHALMOLOGIST</div>
-                        <div class="p-1">ORTHOPAEDIC SURGEON</div>
-                        <div class="p-1">PAEDIATRICIAN</div>
-                        <div class="p-1">PLASTIC SURGEON</div>
-                        <div class="p-1">RADIOLOGIST</div>
-                        <div class="p-1">UROLOGIST</div>
-                        <div class="p-1">PSYCHIATRIST</div>
-                        <div class="row mb-3">
-                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">EmailTETE</label>
-                            <div class="col-sm-10">
-                              <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
-                            </div>
-                          </div>
-                          <div class="row mb-3">
-                            <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">EmailTASDFASDFETE</label>
-                            <div class="col-sm-10">
-                              <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
-                            </div>
-                          </div>
-                    </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">RADIOLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">UROLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PSYCHIATRIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">CARDIOLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">EAR, NOSE & THROAT SURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">GENERAL SURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">NEPHROLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">GASTROENTEROLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">NEUROSURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">OBSTETRICIAN & GYNAECOLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">OPHTHALMOLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">ORTHOPAEDIC SURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PAEDIATRICIAN</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PLASTIC SURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PAEDIATRICIAN</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PLASTIC SURGEON</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">UROLOGIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+            <div class="row pt-1">
+                <div class="col-sm-6 fw-semibold">PSYCHIATRIST</div>
+                <div class="col-sm-6">Dr Anand</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="row border shadow-sm rounded mb-2">
+                <div class="row">
+                    <div class="col-sm-6 fw-semibold">MEDICAL DIRECTOR</div>
+                    <div class="col-sm-6">DR HUE TECK LEE</div>
                 </div>
-                <div class="col">
-                    <div class="d-flex flex-column mb-3">
-                        <div class="p-1">.</div>
-                        <div class="p-1">Flex item 2</div>
-                        <div class="p-1">Flex item 3</div>
+                <div class="row">
+                    <div class="col fs-4"><b>HOSPITAL PERSONNEL ON-CALL</b></div>
+                </div>
+                <div class="row ms-1">
+                    <div class="col-sm-6  fw-semibold">HEAD OF DEPARTMENT</div>
+                    <div class="col-sm-6">RAMLAH</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">MEDICAL OFFICER IN CHARGE</div>
+                    <div class="col-sm-6">DR CHALVAN</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">UNI MANAGER AM</div>
+                    <div class="col-sm-6">SR RAMLAH</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">UNI MANAGER PM</div>
+                    <div class="col-sm-6">SR ROSLINA</div>
+                </div>
+                <div class="row shadow-sm border rounded ms-1 pb-2 mt-2 mb-2 bg-secondary-subtle">
+                    <div class="row border rounded text-white ms-1 mt-1 pt-2 pb-1 bg-secondary fw-bold">
+                        <center>MEDICAL OFFICER</center>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="row fw-semibold ps-3">MORNING</div>
+                            <div class="row fw-semibold ps-3">EVENING</div>
+                            <div class="row fw-semibold ps-3">NIGHT</div>
+                        </div>
+                        <div class="col-4">
+                            <div class="row">1) DR CHALVAN</div>
+                            <div class="row">1) DR CHALVAN</div>
+                            <div class="row">1) DR CHALVAN</div>
+                        </div>
+                        <div class="col-4">
+                            <div class="row">2) DR CHALVAN</div>
+                            <div class="row">2) DR CHALVAN</div>
+                            <div class="row">2) DR CHALVAN</div>
+                        </div>
+                    </div>    
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">LAB 1</div>
+                    <div class="col-sm-6">ZHAFRI</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">LAB 2</div>
+                    <div class="col-sm-6">NURUL ZHAFRI</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">RADIOLOGIST</div>
+                    <div class="col-sm-6">DR ANISHA</div>
+                </div>
+                <div class="row pt-1 ms-1">
+                    <div class="col-sm-6 fw-semibold">RADIOGRAPHER</div>
+                    <div class="col-sm-6">RAJA</div>
+                </div>
+            </div>
+            <!--consultant on-leave-->
+            <div class="row pt-1 pb-1 border shadow-sm rounded">
+                <div class="row">
+                    <div class="col fs-4"><b>CONSULTANT ON LEAVE</b></div>
+                </div>
+                <div class="row pt-1 ms-2 bg-secondary-subtle shadow-sm border rounded">
+                    <div class="col-4">
+                        <div class="row rounded text-white p-1 bg-secondary me-2"><b><center>DOCTOR</center></b></div>
+                        <div class="row"><center>DR RIANA</center></div>
+                        <div class="row"><center>DR MAZITA</center></div>
+                        <div class="row"><center>DR MAZITA</center></div>
+                        <div class="row"><center>DR MAZITA</center></div>
+                        <div class="row"><center>DR MAZITA</center></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="row rounded text-white p-1 bg-secondary me-2"><b><center>DATE</center></b></div>
+                        <div class="row"><center>21/9/2023</center></div>
+                        <div class="row"><center>21/9/2023</center></div>
+                        <div class="row"><center>21/9/2023</center></div>
+                        <div class="row"><center>21/9/2023</center></div>
+                        <div class="row"><center>21/9/2023</center></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="row rounded text-white p-1 bg-secondary me-2"><b><center>COVER BY</center></b></div>
+                        <div class="row"><center>DR VINCENT</center></div>
+                        <div class="row"><center>DR SUBANESH</center></div>
+                        <div class="row"><center>DR SUBANESH</center></div>
+                        <div class="row"><center>DR SUBANESH</center></div>
+                        <div class="row"><center>DR SUBANESH</center></div>
                     </div>
                 </div>
             </div>
-            
-        </div>
-        <div class="col">
-            
         </div>
     </div>
 
@@ -81,8 +200,8 @@
 
 
 
-<footer class="text-center text-white fixed-bottom bg-dark mt-5">
-    <div class="text-center p-2" style="background-color:rgba(0,0,0,0.2);">IT Services @ 2023 Copyright :<a class="text-white text-decoration-none" href="#">A&E Dashboard</a></div>
+<footer class="text-center text-white fixed-bottom bg-dark mt-2">
+    <div class="text-center p-1" style="background-color:rgba(0,0,0,0.2);">IT Services @ 2023 Copyright :<a class="text-white text-decoration-none" href="#">A&E Dashboard</a></div>
 </footer>
     <script src="static/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="Interface/style/jQuery/jquery-3.6.0.min.js"></script>
